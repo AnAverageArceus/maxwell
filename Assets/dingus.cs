f3e9d3b1a56425e25346709c3a8d448d8c041bc4
AnAverageArceus <65923679+AnAverageArceus@users.noreply.github.com>
2024-04-23 12:17:54 -0700

    Initial commit

---
+++ Assets/dingus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+
+public class dingus : MonoBehaviour {
+
+	public KMBombInfo bomb;
+	public KMSelectable playbutton;
+	public KMSelectable patpat;
+	public AudioSource dingusnoise;
+	public AudioClip music;
+	public AudioClip miau;
+
+	bool playing;
+
+	void Start () {
+		playbutton.OnInteract += delegate () {
+			PlayMusic ();
+			return false;
+		};
+		patpat.OnInteract += delegate () {
+			Meow ();
+			return false;
+		};
+		dingusnoise.clip = music;
+		dingusnoise.loop = true;
+	}
+
+	void PlayMusic(){
+		if (playing) {
+			dingusnoise.Stop ();
+			playing = false;
+		} else {
+			dingusnoise.Play ();
+			playing = true;
+		}
+	}
+	void Meow(){
+		dingusnoise.PlayOneShot (miau);
+	}
+}
